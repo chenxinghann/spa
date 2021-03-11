e3caeba3d67062efab618cdaa8e61617fbb8a55b
chenxinghann <1435479950@qq.com>
2021-03-11 15:16:25 +0800

    message

---
+++ 12-range/main.js
@@ -0,0 +1,12 @@
+$(function(){
+  //get elem:range,span
+  //range change event
+    //get range value
+    //set span text
+  let $range=$('input'),
+      $age=$('span');
+  
+  $range.change(()=>{
+    $age.html($range.val());
+  })
+});
